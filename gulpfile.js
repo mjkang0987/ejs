b1004d15b2029d862f863e2f9c7e4b96f61e393e
minjeongkang <snowdrop1202@gmail.com>
2021-03-04 23:33:26 +0900

    feat: gulp scripts compile add

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const gulp = require('gulp');
 const scss = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const del = require('del');
 
 const PATH = {
   HTML: '/views',
   STYLES: './styles',
   IMAGES: './images',
+  SCRIPTS: './scripts',
   DIST: './dist',
   _HTML: '/views',
   _STYLES: '/styles',
-  _IMAGES: '/images'
+  _IMAGES: '/images',
+  _SCRIPTS: '/scripts'
 };
 
-const {HTML, STYLES, IMAGES, DIST, _HTML, _STYLES, _IMAGES} = PATH
+const {HTML, STYLES, IMAGES, SCRIPTS, DIST, _HTML, _STYLES, _IMAGES, _SCRIPTS} = PATH
 
 gulp.task('clean', () => {
   return new Promise(resolve => {
@@ -35,4 +39,12 @@ gulp.task('styles', () => {
     resolve();
   });
 });
-gulp.task('default', gulp.series(['styles']));
+
+gulp.task('scripts', () => {
+  return new Promise(resolve => {
+    gulp.src(`${SCRIPTS}/*.js`)
+      .pipe(uglify({mangle: true}))
+      .pipe(gulp.dest(`${DIST}${_SCRIPTS}`));
+    resolve();
+  });
+});
