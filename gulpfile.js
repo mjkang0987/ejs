2bc6b53331b9b64d4a3662d5c701500ec636137b
minjeongkang <snowdrop1202@gmail.com>
2021-03-20 12:41:55 +0900

    refactor: 폴더 경로 상수 변경

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,12 @@ const rename = require('gulp-rename');
 const del = require('del');
 
 const PATH = {
-  HTML: '/views',
-  STYLES: './styles',
-  IMAGES: './images',
-  SCRIPTS: './scripts',
-  DIST: './dist',
-  _HTML: '/views',
-  _STYLES: '/styles',
-  _IMAGES: '/images',
-  _SCRIPTS: '/scripts'
+  HTML: 'views',
+  STYLES: 'styles',
+  IMAGES: 'images',
+  SCRIPTS: 'scripts',
+  JSON: 'data',
+  DIST: 'dist'
 };
 
 const {HTML, STYLES, IMAGES, SCRIPTS, DIST, _HTML, _STYLES, _IMAGES, _SCRIPTS} = PATH
@@ -42,6 +39,7 @@ gulp.task('styles', () => {
       indentWidth: 4,
       precision: 8,
       sourceComments: false
+const {HTML, STYLES, IMAGES, SCRIPTS, JSON, DIST} = PATH;
     };
     gulp.src(`${STYLES}/*.scss`)
       .pipe(scss(options))
