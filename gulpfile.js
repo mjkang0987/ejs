4649cd4dea0e3fd55d2d66a3c0cf391372a14f78
minjeongkang <snowdrop1202@gmail.com>
2021-03-20 12:43:24 +0900

    refactor: gulp task 삭제

--- gulpfile.js
+++ gulpfile.js
@@ -13,46 +13,5 @@ const PATH = {
   DIST: 'dist'
 };
 
-const {HTML, STYLES, IMAGES, SCRIPTS, DIST, _HTML, _STYLES, _IMAGES, _SCRIPTS} = PATH
-
-gulp.task('clean', () => {
-  return new Promise(resolve => {
-    del.sync(DIST, {force: true});
-    resolve();
-  });
-});
-
-gulp.task('html', () => {
-  return new Promise(resolve => {
-    gulp.src(`${HTML}/*.ejs`)
-      .pipe(rename({ extname: '.html' }))
-      .pipe(gulp.dest(`${DIST}${_HTML}`));
-    resolve();
-  });
-});
-
-gulp.task('styles', () => {
-  return new Promise(resolve => {
-    const options = {
-      outputStyle: 'nested',
-      indentType: 'space',
-      indentWidth: 4,
-      precision: 8,
-      sourceComments: false
 const {HTML, STYLES, IMAGES, SCRIPTS, JSON, DIST} = PATH;
     };
-    gulp.src(`${STYLES}/*.scss`)
-      .pipe(scss(options))
-      .pipe(gulp.dest(`${DIST}${_STYLES}`));
-    resolve();
-  });
-});
-
-gulp.task('scripts', () => {
-  return new Promise(resolve => {
-    gulp.src(`${SCRIPTS}/*.js`)
-      .pipe(uglify({mangle: true}))
-      .pipe(gulp.dest(`${DIST}${_SCRIPTS}`));
-    resolve();
-  });
-});
