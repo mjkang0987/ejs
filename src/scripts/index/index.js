af154fd08033caebb2e3fba4b687292e363c13f2
mjkang0987 <snowdrop1202@gmail.com>
2021-06-02 14:36:13 +0900

    feat: index/index.js 추가

--- src/scripts/index/index.js
+++ src/scripts/index/index.js
@@ -1,6 +1,34 @@
-const lost = _a => {
-  console.log('korea');
-  console.log('korea');
-  console.log('korea');
-  console.log('korea');
+const docSelector = ({
+  el,
+  all: isAll
+}) => {
+  if (isAll) {
+    return document.querySelectorAll(el);
+  } else {
+    return document.querySelector(el);
+  }
 };
+
+const createEl = ({tag, attribute}) => {
+  const el = document.createElement(tag);
+  Object.assign(el, attribute);
+  return el;
+};
+
+const insertEl = ({
+  target,
+  position: pos = 'beforebegin',
+  el
+}) => {
+  target.insertAdjacentElement(pos, el);
+};
+
+const init = _ => {
+  console.log('dom ready');
+};
+
+if (document.readyState === 'complete') {
+  init();
+} else if (document.addEventListener) {
+  document.addEventListener('DOMContentLoaded', init);
+}
